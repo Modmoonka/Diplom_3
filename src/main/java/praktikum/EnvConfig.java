cf56f1b1b1e1e712e88c0b7ebfa3e6c32c847704
BabatenkoTO <tanya-mongush@mail.ru>
2025-07-22 22:57:14 +0700

    add EnvCongig

---
+++ src/main/java/praktikum/EnvConfig.java
@@ -0,0 +1,12 @@
+package praktikum;
+
+public class EnvConfig {
+        public static final String BASE_URL = "https://stellarburgers.nomoreparties.site";
+        public static final String REGISTER_PAGE_URL = "https://stellarburgers.nomoreparties.site/register";
+        public static final String FORGOT_PASSWORD_URL = "https://stellarburgers.nomoreparties.site/forgot-password";
+        public static final String CREATE_USER_ENDPOINT = "/api/auth/register";
+        public static final String LOGIN_USER_ENDPOINT = "/api/auth/login";
+        public static final String USER_ENDPOINT = "/api/auth/user";
+        public static final int EXPLICIT_WAIT = 5;
+        //public static final int IMPLICIT_WAIT = 5;
+}
